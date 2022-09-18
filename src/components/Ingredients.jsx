e64256e8054624ea7488b21eb5afe4c0ff65dd72
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-09-17 22:41:12 -0400

    testing ingredient display

---
+++ src/components/Ingredients.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+function Ingredients(data) {
+  const [ingredients, setIngredients] = useState(data);
+  console.log(ingredients);
+
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+export default Ingredients;
--- src/pages/Recipie.jsx
+++ src/pages/Recipie.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
+import Ingredients from "../components/Ingredients";
 
 function Recipie() {
   let params = useParams();
   const [recipe, setRecipe] = useState({});
-  const [getIngredients, setIngredients] = useState();
 
   useEffect(() => {
     getRecepie(params.recipe);
@@ -22,14 +23,40 @@ function Recipie() {
   };
 
   return (
-    <div>
+    <RecipeDisplay>
+      <h1>{recipe.title}</h1>
       <img src={recipe.image}></img>
-      <h3 dangerouslySetInnerHTML={{ __html: recipe.summary }} />
-      <h3>{recipe.title}</h3>
-      <h3 dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
-      <h3></h3>
-    </div>
+      <RecipeContent>
+        <h3 dangerouslySetInnerHTML={{ __html: recipe.summary }} />
+        <h3 dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
+        <Ingredients data={recipe.extendedIngredients} />
+      </RecipeContent>
+    </RecipeDisplay>
   );
 }
+
+const RecipeDisplay = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 5px solid black;
+  border-radius: 10px;
+  padding: 3rem;
+
+  img {
+    margin: 1rem;
+    border-radius: 10px;
+  }
+
+  h3 {
+    padding: 1rem;
+  }
+`;
+
+const RecipeContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  padding: 3rem;
+`;
 
 export default Recipie;
